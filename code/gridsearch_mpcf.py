137ec0a084902227d54d19313d7d33031cdc18b8
Marco Unternährer <marco.unter@gmail.com>
2016-05-12 19:44:09 +0200

    gridsearch for ml-1m

--- code/gridsearch_mpcf.py
+++ code/gridsearch_mpcf.py
@@ -24,18 +24,18 @@ if __name__ == '__main__':
     os.chdir(dname)
 
     grid_search = False
-    nb_random_samples = 100
+    nb_random_samples = 20
     cores = multiprocessing.cpu_count()
 
     params = {
-        'lr': [0.001, 0.003, 0.01, 0.03],
-        'lr_decay': [5e-4, 2e-2, 3e-2],
+        'lr': [0.003, 0.01, 0.03],
+        'lr_decay': [5e-4, 2e-2],
         'reg_lambda': [0.001, 0.003, 0.01],
         'nb_latent_f': [64, 96, 128],
-        'nb_user_pref': [2, 4, 8, 16],
+        'nb_user_pref': [2, 4, 8],
         'binarize': [True, False],
         'use_avg_rating': [True, False],
-        'zero_sample_factor': [3, 4, 5],
+        'zero_sample_factor': [3, 5],
     }
 
     param_comb = list(ParameterGrid(params))
@@ -43,19 +43,25 @@ if __name__ == '__main__':
     if not grid_search: # random search
         param_comb = random.sample(param_comb, nb_random_samples)
 
+    experiment_name = 'no-si_ml-1m_e{}_tt-0.2_task-{}'
+
     config = {}
     config['init_params_scale'] = 0.001
-    config['nb_epochs'] = 100
-    config['ratings_path'] = 'data/splits/ml-100k/ratings.csv'
+    config['nb_epochs'] = 20
+    config['ratings_path'] = 'data/splits/ml-1m/ratings.csv'
     config['sparse_item'] = True
-    config['train_test_split'] = 0.7
-    config['train_path'] = 'data/splits/ml-100k/sparse-item/0.7-0.8-train.csv'
-    config['test_path'] = 'data/splits/ml-100k/sparse-item/0.7-test.csv'
+    config['train_test_split'] = 0.2
+    config['train_path'] = 'data/splits/ml-1m/sparse-item/0.2-train.csv'
+    config['test_path'] = 'data/splits/ml-1m/sparse-item/0.2-test.csv'
     config['test'] = True
-    config['val'] = True
+    config['val'] = False
     if config['val']:
         config['train_val_split'] = 0.8
         config['val_path'] = 'data/splits/ml-100k/sparse-item/0.7-0.8-val.csv'
+
+    config['adagrad'] = True
+    if config['adagrad']:
+        config['ada_eps'] = 1e-6
 
     config['model_save_dir'] = 'models/mpcf'
     config['metrics_save_dir'] = 'metrics/mpcf'
@@ -69,7 +75,6 @@ if __name__ == '__main__':
     config['top_n_predictions'] = 100
     config['run_movie_metrics'] = False
     config['eval_in_parallel'] = False
-    config['pool_size'] = 2
 
     all_configs = []
     for i, p in enumerate(param_comb):
@@ -80,7 +85,7 @@ if __name__ == '__main__':
             curr_config['binarize_pos'] = 1
             curr_config['binarize_neg'] = 0
 
-        curr_config['experiment_name'] = 'no-si_ml-100k_e{}_tt-0.7_task-{}'.format(curr_config['nb_epochs'], i)
+        curr_config['experiment_name'] = experiment_name.format(curr_config['nb_epochs'], i)
 
         all_configs.append(curr_config)
 
